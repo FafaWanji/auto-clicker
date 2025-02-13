5d10c336ea8c41bbf9d44cdd8b516e630972afdb
Fahrettin Ayaz <f.ayaz@outlook.de>
2025-02-13 14:35:00 +0100

    added click for staging / made faster

--- auto_downloader.py
+++ auto_downloader.py
@@ -7,8 +7,9 @@ print(os.getcwd())
 pyautogui.FAILSAFE = False
 
 # Image file paths
-launcher_download_path = "download_launcher.PNG"
-website_download_path = "download_website.PNG"
+launcher_download_path = "launcher.PNG"
+website_download_path = "website.PNG"
+staging_download_path = "staging.PNG"
 threshold = 0.9
 
 while True:
@@ -21,12 +22,12 @@ while True:
             pyautogui.click(launcher_location.left + 5, launcher_location.top + 5)
 
         # Introduce a delay before taking the screenshot
-        time.sleep(3)
+        time.sleep(1)
     
     except pyautogui.ImageNotFoundException:
         # Handle the exception (e.g., print a message)
         print("Image not found. Waiting for the element to appear.")
-        time.sleep(3)  # Add a delay before retrying
+        time.sleep(1)  # Add a delay before retrying
 
     try:
         print("Checking for Website Download Button")
@@ -36,9 +37,24 @@ while True:
             # Click on the website download button if found
             pyautogui.click(website_location.left + 5, website_location.top + 5)
 
-        time.sleep(3)
+        time.sleep(1)
         print("Waiting for 5 seconds...")
     except pyautogui.ImageNotFoundException:
         # Handle the exception (e.g., print a message)
         print("Image not found. Waiting for the element to appear.")
-        time.sleep(3)  # Add a delay before retrying
+        time.sleep(1)  # Add a delay before retrying
+    
+    try:
+        print("Checking for Staging Button")
+        staging_location = pyautogui.locateOnScreen(staging_download_path, confidence=threshold)
+
+        if staging_location is not None:
+            pyautogui.click(staging_location.left + 5, staging_location.top + 5)
+
+        # Introduce a delay before taking the screenshot
+        time.sleep(1)
+    
+    except pyautogui.ImageNotFoundException:
+        # Handle the exception (e.g., print a message)
+        print("Image not found. Waiting for the element to appear.")
+        time.sleep(1)  # Add a delay before retrying
